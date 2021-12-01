65e20e47b086d4f67b63afd69b84f7c5d9137622
alex <yuting09120310@hotmail.com>
2021-12-01 13:54:16 +0800

    防呆

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 window.onload = function(){
   document.getElementById('getData').onclick = function(){
     let search = document.getElementById('floatingInput').value;
-    chrome.tabs.create({url: 'https://www.youtube.com/results?search_query=' + encodeURI(search)});
+    if(search.length > 0){
+      chrome.tabs.create({url: 'https://www.youtube.com/results?search_query=' + encodeURI(search)});
+    }
   }
 }
